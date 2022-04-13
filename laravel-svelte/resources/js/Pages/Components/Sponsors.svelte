b789df7a4aed4978e15cff5bfa191395b2b00df8
MCKevmeister <markchristison@gmail.com>
2022-04-13 20:33:59 +1200

    update to use generic logos

--- laravel-svelte/resources/js/Pages/Components/Sponsors.svelte
+++ laravel-svelte/resources/js/Pages/Components/Sponsors.svelte
@@ -3,22 +3,22 @@
             <p class="mt-6 text-center text-3xl font-extrabold text-gray-900">We are Proudly Sponsored by</p>
         <div class="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-8">
             <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-                <img class="max-h-20" src="https://static.countdown.co.nz/assets/images/Header/countdown-desktop-logo.svg" alt="Countdown">
+                <img class="max-h-20" src="https://raw.githubusercontent.com/MCKevmeister/web701/main/laravel-svelte/resources/images/food_truck_food.png" alt="Food Truck Food">
             </div>
             <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-                <img class="max-h-20" src="https://councilmark.co.nz/assets/CouncilLogos/NCC-Logo-Portrait-RGB.png" alt="Nelson City Council">
+                <img class="max-h-20" src="https://raw.githubusercontent.com/MCKevmeister/web701/main/laravel-svelte/resources/images/Nelson_food.png" alt="Nelson Food">
             </div>
             <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-                <img class="max-h-20" src="https://www.tasman.govt.nz/assets/PageContentImages/Logos/TDC-Bilingual-Logo-Wide-Colour-RGB-PNG-high-resolution.png" alt="Tasman District Council">
+                <img class="max-h-20" src="https://raw.githubusercontent.com/MCKevmeister/web701/main/laravel-svelte/resources/images/generic_company_logo.png" alt="ABC generic company">
             </div>
             <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-                <img class="max-h-20" src="https://iacctexas.cividesk.com/files/civicrm/persist/contribute/images/your%20logo%20here.png" alt="Your Logo Here">
+<!--                <img class="max-h-20" src="https://iacctexas.cividesk.com/files/civicrm/persist/contribute/images/your%20logo%20here.png" alt="Your Logo Here">-->
             </div>
             <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-                <img class="max-h-20" src="https://iacctexas.cividesk.com/files/civicrm/persist/contribute/images/your%20logo%20here.png" alt="Your Logo Here">
+                <img class="max-h-20" src="https://raw.githubusercontent.com/MCKevmeister/web701/main/laravel-svelte/resources/images/your%20logo%20here.png" alt="Your Logo Here">
             </div>
             <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-                <img class="max-h-20" src="https://iacctexas.cividesk.com/files/civicrm/persist/contribute/images/your%20logo%20here.png" alt="Your Logo Here">
+<!--                <img class="max-h-20" src="https://iacctexas.cividesk.com/files/civicrm/persist/contribute/images/your%20logo%20here.png" alt="Your Logo Here">-->
             </div>
         </div>
     </div>
