afd04f5caa028bcffa955daa1ec11553a122767e
MCKevmeister <markchristison@gmail.com>
2022-05-04 11:40:50 +1200

    minor tweaks

--- laravel-svelte/resources/js/Pages/Components/Logo.svelte
+++ laravel-svelte/resources/js/Pages/Components/Logo.svelte
@@ -5,7 +5,6 @@
 <div class="bg-white">
     <div class="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:py-4 lg:px-4">
         <p class="mt-6 text-center text-3xl font-extrabold text-gray-900">Welcome to the Food Charity!</p>
-
     </div>
     <img class="mx-auto block align-content-center w-auto" src={foodCharityLogo} alt="Food Charity Logo" />
 </div>
