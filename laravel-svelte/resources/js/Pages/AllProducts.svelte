6ad0c3515a512df8c9ea2bda24b41b292d2a3316
MCKevmeister <markchristison@gmail.com>
2022-05-07 00:28:40 +1200

    update svelte files

--- laravel-svelte/resources/js/Pages/AllProducts.svelte
+++ laravel-svelte/resources/js/Pages/AllProducts.svelte
@@ -1,29 +1,37 @@
 <script>
     import ProductCard from "./Components/ProductCard.svelte";
     import NavBar from "./Components/NavBar.svelte";
+    import {Inertia} from "@inertiajs/inertia";
+    import {throttle} from "lodash";
 
-    //TODO: Replace hard coded data with API data
-    let imageSource = "https://images.pexels.com/photos/704569/pexels-photo-704569.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
-    let productName = "Food Basket";
-    let productPrice = "100";
-    let productLink = "https://www.google.com";
+    // //TODO: Replace hard coded data with API data
+    // let imageSource = "https://images.pexels.com/photos/704569/pexels-photo-704569.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
+    // let productName = "Food Basket";
+    // let productPrice = "100";
+    // let productLink = "https://www.google.com";
 
-    let products = [
-        {imageSource, productName, productPrice, productLink},
-        {imageSource, productName, productPrice, productLink},
-        {imageSource, productName, productPrice, productLink},
-        {imageSource, productName, productPrice, productLink},
-        {imageSource, productName, productPrice, productLink},
-        {imageSource, productName, productPrice, productLink},
-        {imageSource, productName, productPrice, productLink},
-        {imageSource, productName, productPrice, productLink},
-    ]
+    export let products;
+    export let searchQuery;
+    export let auth;
+
+    const search = throttle(() => {
+        Inertia.get('/products', {searchQuery}, {
+            preserveState: true,
+            replace: true
+        });
+    }, 500);
 </script>
 
 <NavBar/>
 
 <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
     <h2 class="text-3xl font-extrabold tracking-tight text-gray-900">Products</h2>
+
+    <!--Search Bar-->
+    <div class="search text-right mr-12 font-medium text-xl mt-10 2xs:mt-3 2xs:mr-0 2xs:text-center">
+        <input type="text" placeholder="Search..." class="border px-2 rounded-lg" bind:value={$searchQuery}>
+    </div>
+
     <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
         {#each products as product}
             <ProductCard {...product}/>
