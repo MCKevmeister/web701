30599d2761c5b3d86625ff5fa21c9e91af6410c2
MCKevmeister <markchristison@gmail.com>
2022-05-07 00:28:55 +1200

    changes to migrations

--- laravel-svelte/database/migrations/2022_05_06_033905_create_tokens_table.php
+++ laravel-svelte/database/migrations/2022_05_06_033905_create_tokens_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Enum\AccountTypeEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,15 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tokens', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('account_type_enum')->default(AccountTypeEnum::BENEFICIARY);
-            $table->rememberToken();
             $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -33,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tokens');
     }
 };
