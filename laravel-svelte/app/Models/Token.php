0aeb0da01a67d27a43f7c91ff65744841ed7043f
MCKevmeister <markchristison@gmail.com>
2022-05-09 16:56:46 +1200

    update token model

--- laravel-svelte/app/Models/Token.php
+++ laravel-svelte/app/Models/Token.php
@@ -5,14 +5,20 @@ namespace App\Models;
 use Database\Factories\TokenFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Token extends Model
 {
     use HasFactory;
 
-    protected $table = 'employees';
+    protected $table = 'tokens';
 
     protected $guarded = [];
+
+    public function assignedTo(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 
     protected static function newFactory()
     {
