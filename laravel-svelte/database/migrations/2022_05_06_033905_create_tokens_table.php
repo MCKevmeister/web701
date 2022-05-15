351dc6e9fefb70e4da53f1ac56c93b1803047cb3
MCKevmeister <markchristison@gmail.com>
2022-05-15 19:21:50 +1200

    add fortify to route service provider

--- laravel-svelte/database/migrations/2022_05_06_033905_create_tokens_table.php
+++ laravel-svelte/database/migrations/2022_05_06_033905_create_tokens_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->unsignedBigInteger('user_id');
-
+            $table->integer('value');
             $table->foreign('user_id')->references('id')->on('users');
         });
     }
